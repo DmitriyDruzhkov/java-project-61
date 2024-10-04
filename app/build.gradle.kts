5ba12d611e14018e685f741be9de95485f9bff16
Dmitriy Druzhkov <sedyar@gmail.com>
2024-10-04 20:24:20 +0300

    3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,6 +14,10 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
+application {
+    // Входная точка
+    mainClass.set("hexlet.code.App")
+}
 
 tasks.test {
     useJUnitPlatform()
