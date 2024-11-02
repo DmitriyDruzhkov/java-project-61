6f9869d3591216794d04bb9f0374d2455c9beccb
DmitriyDruzhkov <sedyar@gmail.com>
2024-11-02 21:41:14 +0300

    Update Engine.java

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 public class Engine {
     private static String name;
     private static Scanner scanner = new Scanner(System.in);
-    public static final int countround = 3;
+    public static final int COUNT_ROUND = 3;
     public static void game(String[][] data, String rule) {
         System.out.println("May I have your name?");
         name = scanner.nextLine();
         System.out.println("Hello, " + name + "!");
         System.out.println(rule);
-        for (int i = 0; i < countround; i++) {
+        for (int i = 0; i < COUNT_ROUND; i++) {
             System.out.println("Question: " + data[i][0]);
             System.out.print("Your answer: ");
             String result = scanner.nextLine();
