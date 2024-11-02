e4cc592debb9a6341db0389293e9face060489ba
DmitriyDruzhkov <sedyar@gmail.com>
2024-11-02 15:05:22 +0300

    Update Utils.java

--- app/src/main/java/hexlet/code/Utils.java
+++ app/src/main/java/hexlet/code/Utils.java
@@ -2,6 +2,9 @@ package hexlet.code;
 import java.util.Random;
 public class Utils {
     public static final int HUNDRED = 100;
+    public static final int FIVE = 5;
+    public static final int TEN = 10;
+    public static final int TWENTY = 20;
     public static int getRandomInt(int min, int max) {
         Random rand = new Random();
         return rand.nextInt(min, max);
